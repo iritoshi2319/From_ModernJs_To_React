33dc305597f39a5c3c29d317d1180fed1af31dea
Toshihiko Irishio <72386294+iritoshi2319@users.noreply.github.com>
2023-10-15 16:13:47 +0000

    hoge

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
+hogehjoge
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
